21730ac3a0c0ae299e4e03e3483e001d34a51e40
radenmasabdul <abdul.tamsis@gmail.com>
2024-02-22 15:35:21 +0700

    add footer, hero, section

---
+++ src/components/layout/Section.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <div class="flex flex-wrap justify-between items-center gap-4 bg-black p-7">
+    <div>
+      <h1 class="text-white">ORIGINAL</h1>
+      <p class="text-white">Watch shows and movies, originals and more.</p>
+    </div>
+    <div>
+      <h1 class="text-white">ENTERTAINMENT</h1>
+      <p class="text-white">Thousand of movies, series, documentary, and more.</p>
+    </div>
+    <div>
+      <h1 class="text-white">FAVORITES</h1>
+      <p class="text-white">From Hollywood movies to Korean dramas, and more.</p>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
