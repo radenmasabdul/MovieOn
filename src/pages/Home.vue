f6516db71e5b6a77672a0a7f218517903ab83058
radenmasabdul <abdul.tamsis@gmail.com>
2024-03-07 23:32:06 +0700

    new section

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Hero from "../components/Hero.vue";
+import Section from "../components/Section.vue";
 </script>
 
 <template>
   <Hero />
+  <Section />
 </template>
 
 <style scoped></style>
