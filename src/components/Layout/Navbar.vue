0fb4425500d06a33c3e06e2916b041709421515d
radenmasabdul <abdul.tamsis@gmail.com>
2024-03-15 23:13:04 +0700

    add hero for carousel

--- src/components/Layout/Navbar.vue
+++ src/components/Layout/Navbar.vue
@@ -5,7 +5,16 @@ import router from "../../routes";
 
 import { ref, onBeforeMount } from "vue";
 
+let dataPopular = ref([]);
+let posterImages = ref("");
+let titleMovies = ref("");
+let overviewMovies = ref("");
 let dataToken = ref("");
+let currentIndex = ref(0);
+let intervalId = ref(null);
+
+let isScrollNavbar = ref(false);
+let isScrollText = ref(false);
 
 const requestToken = async () => {
   try {
@@ -42,6 +51,38 @@ const checkTokenValidity = async () => {
   }
 };
 
+const getPopularMovies = async () => {
+  try {
+    let payload = {
+      api_key: import.meta.env.VITE_TMDB_KEY,
+    };
+    const res = await Api.get("/popular", {
+      params: payload,
+    });
+    dataPopular.value = res.data.results;
+
+    dataPopular.value.forEach((item) => {
+      posterImages.value = item.poster_path;
+      titleMovies.value = item.title;
+      overviewMovies.value = item.overview;
+    });
+
+    if (dataPopular.value.length > 0) {
+      posterImages.value = `https://image.tmdb.org/t/p/original${dataPopular.value[0].poster_path}`;
+    }
+
+    intervalId.value = setInterval(changePoster, 10000);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+onBeforeMount(async () => {
+  await checkTokenValidity();
+  getPopularMovies();
+  window.addEventListener("scroll", handleScroll);
+});
+
 const Logout = async () => {
   try {
     localStorage.removeItem("token_expiration_timestamp");
@@ -68,19 +109,47 @@ const Logout = async () => {
   }
 };
 
-onBeforeMount(async () => {
-  await checkTokenValidity();
-});
+const handleScroll = () => {
+  const heroElement = document.querySelector(".hero");
+  const navbar = document.querySelector(".navbar.headers");
+
+  if (heroElement && navbar) {
+    const heroHeight = heroElement.offsetHeight;
+    const scrollTop = window.scrollY;
+
+    if (scrollTop > heroHeight) {
+      isScrollNavbar.value = true;
+      isScrollText.value = true;
+    } else {
+      isScrollNavbar.value = false;
+      isScrollText.value = false;
+    }
+  }
+};
+
+const changePoster = () => {
+  if (dataPopular.value.length > 0) {
+    currentIndex.value = (currentIndex.value + 1) % dataPopular.value.length;
+    posterImages.value = `https://image.tmdb.org/t/p/original${dataPopular.value[currentIndex.value].poster_path}`;
+    titleMovies.value = dataPopular.value[currentIndex.value].title;
+    overviewMovies.value = dataPopular.value[currentIndex.value].overview;
+  }
+};
 </script>
 
 <template>
-  <div class="navbar bg-base-100">
+  <div class="navbar headers" :class="{ 'navbar-white': isScrollNavbar }">
     <div class="flex-1">
-      <a class="btn btn-ghost text-xl">daisyUI</a>
+      <p class="text-2xl text-white font-JakartaSans font-extrabold" :class="{ 'text-black': isScrollText }">
+        Movie On!
+      </p>
     </div>
     <div class="flex-none gap-2">
       <div class="form-control">
-        <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+        <input
+          type="text"
+          class="bg-transparent border-2 border-white text-white font-JakartaSans text-sm rounded-lg input-primary input-bordered w-24 md:w-auto focus:ring-blue-700 block p-2.5"
+        />
       </div>
       <div class="dropdown dropdown-end">
         <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
@@ -93,17 +162,45 @@ onBeforeMount(async () => {
         </div>
         <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
           <li>
-            <a class="justify-between">
-              Profile
-              <span class="badge">New</span>
-            </a>
+            <button class="justify-between">Profile</button>
           </li>
-          <li><a>Settings</a></li>
+          <li><button>Settings</button></li>
           <li><button @click.prevent="Logout()">Logout</button></li>
         </ul>
       </div>
     </div>
   </div>
+
+  <div class="hero min-h-screen" :style="{ backgroundImage: `url(${posterImages})` }">
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold text-white font-JakartaSans">{{ titleMovies }}</h1>
+        <p class="mb-5 text-white font-JakartaSans font-medium">{{ overviewMovies }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.headers {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: rgba(255, 255, 255, 0.296);
+  z-index: 1000;
+}
+
+.hero {
+  transition: background-image 0.5s ease-in-out;
+}
+
+.navbar-white {
+  background-color: white;
+  transition: background-color 0.5s linear;
+}
+
+.text-black {
+  color: black;
+}
+</style>
