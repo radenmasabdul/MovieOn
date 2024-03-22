3fa48c2cd2d5e8b254138bf0dc782aba87e55de6
radenmasabdul <abdul.tamsis@gmail.com>
2024-03-23 01:33:43 +0700

    mapping pada list now playing

--- src/components/Layout/Card.vue
+++ src/components/Layout/Card.vue
@@ -1,22 +1,52 @@
 <script setup>
+import { usenowPlayingStore } from "../../utils/stores/nowPlaying";
+
+import { onBeforeMount, computed } from "vue";
+
+const dataNowPlaying = computed(() => store.getnowPlaying);
+
+const store = usenowPlayingStore();
+
 const props = defineProps({
   activeTab: String,
 });
+
+const fetchNowPlaying = async () => {
+  await store.fetchNowPlaying();
+};
+
+onBeforeMount(() => {
+  fetchNowPlaying();
+});
+
+const getMoviePoster = (movie) => {
+  return `https://image.tmdb.org/t/p/original${movie.poster_path}`;
+};
 </script>
 
 <template>
-  <div>
-    <div class="card w-96 bg-base-100 shadow-xl" v-if="activeTab === 'Movies'">
-      <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-      <div class="card-body">
-        <h2 class="card-title">MOVIE!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
+  <div v-if="activeTab === 'Movies'">
+    <div class="py-2">
+      <div class="mx-2">
+        <h1 class="text-white font-JakartaSans font-bold text-2xl">Now Playing</h1>
+      </div>
+
+      <div class="flex flex-wrap justify-center items-center gap-4 py-2">
+        <div v-for="(movie, index) in dataNowPlaying" :key="index" class="card w-96 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <figure class="img"><img :src="getMoviePoster(movie)" :alt="movie.title" /></figure>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.card-body {
+  padding: 2%;
+}
+.img {
+  border-radius: 1%;
+}
+</style>
