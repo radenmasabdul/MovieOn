1496345e02a9b92d700d06f76d91c569ec65edb8
radenmasabdul <abdul.tamsis@gmail.com>
2024-03-28 00:43:19 +0700

    clear comment

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,24 +3,15 @@ import Modal from "./Layout/Modal.vue";
 
 import { usenowPlayingStore } from "../utils/stores/nowPlaying.js";
 
-import { ref, onBeforeMount, computed } from "vue";
+import { ref, onBeforeMount } from "vue";
 
 const store = usenowPlayingStore();
-
-// const dataNowPlaying = computed(() => store.getnowPlaying);
-// const posterImages = computed(() => store.getPosterImages);
-// const titleMovies = computed(() => store.getTitleMovies);
-// const overviewMovies = computed(() => store.getOverviewMovies);
 
 let currentIndex = ref(0);
 let intervalId = ref(null);
 
 let isScrollNavbar = ref(false);
 let isScrollText = ref(false);
-
-// const fetchNowPlaying = async () => {
-//   await store.fetchNowPlaying();
-// };
 
 const changePoster = () => {
   if (store.getnowPlaying.length > 0) {
@@ -57,31 +48,6 @@ onBeforeMount(async () => {
   window.addEventListener("scroll", handleScroll);
   intervalId.value = setInterval(changePoster, 5000);
 });
-
-// const changePoster = () => {
-//   currentIndex.value = (currentIndex.value + 1) % dataNowPlaying.value.length;
-//   store.posterImages = `https://image.tmdb.org/t/p/original${dataNowPlaying.value[currentIndex.value].poster_path}`;
-//   store.titleMovies = dataNowPlaying.value[currentIndex.value].title;
-//   store.overviewMovies = dataNowPlaying.value[currentIndex.value].overview;
-// };
-
-// const handleScroll = () => {
-//   const heroElement = document.querySelector(".hero");
-//   const navbar = document.querySelector(".navbar.headers");
-
-//   if (heroElement && navbar) {
-//     const heroHeight = heroElement.offsetHeight;
-//     const scrollTop = window.scrollY;
-
-//     if (scrollTop > heroHeight) {
-//       isScrollNavbar.value = true;
-//       isScrollText.value = true;
-//     } else {
-//       isScrollNavbar.value = false;
-//       isScrollText.value = false;
-//     }
-//   }
-// };
 </script>
 
 <template>
